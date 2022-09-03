23e149dceed2a96c09587eb61bb637d7b923e1ba
AmdjedSanero <amdjed.sanero@gmail.com>
2022-09-03 19:01:45 +0100

    s

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let main = document.querySelector("main");
 
 function getData() {
-  fetch("../data.json")
+  fetch("./data.json")
     .then((response) => response.json())
     .then((myData) => {
       myData.forEach((element) => {
